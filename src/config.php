6b0b5038aaf2081eed82567a30e35a551966b6b1
nstCactus <nstCactus@users.noreply.github.com>
2019-07-01 15:21:29 +0200

    Fix documentation about config file name

--- src/config.php
+++ src/config.php
@@ -15,7 +15,7 @@
  * This file exists only as a template for the Craft Contact Form Extensions settings.
  * It does nothing on its own.
  *
- * Don't edit this file, instead copy it to 'craft/config' as 'craft-contact-form-extensions.php'
+ * Don't edit this file, instead copy it to 'craft/config' as 'contact-form-extensions.php'
  * and make your changes there to override default settings.
  *
  * Once copied to 'craft/config', this file will be multi-environment aware as
